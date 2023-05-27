e181739a91d269f9b3a6fc74c6e3967b6947110b
Gizat Uteev <gizat.uteev@gmail.com>
2023-05-27 14:35:32 +0300

    v0.921 face_id component restruction

--- src/components/FaceID/index.jsx
+++ src/components/FaceID/index.jsx
@@ -17,54 +17,59 @@ import styles from './FaceID.module.scss';
 import { Div } from '../UI/Div';
 import { Submit } from '../UI/Submit';
 
-import {
-    generatePinnedFaceEncodings,
-    decryptData
-} from '../../utils/crypto';
+import { generatePinnedFaceEncodings, decryptData } from '../../utils/crypto';
+
+// Constants
+const MODEL_PATH = "/models";
+const VIDEO_CONSTRAINTS = {
+    width: 640,
+    height: 480,
+    facingMode: 'user'
+};
+const FACE_DETECTION_OPTIONS = new faceapi.TinyFaceDetectorOptions();
+const CAMERA_INTERVAL = 100;
+
+// Subcomponents
+const FaceIDMessage = ({ showMessage, isMultipleFacesDetected }) => (
+    <div className={`${styles.faceid_message} ${showMessage || isMultipleFacesDetected ? styles.message_active : ''}`}>
+        {showMessage}
+    </div>
+);
 
 
 const FaceID = () => {
 
+    // Initialization
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const webcamRef = useRef(null);
     const canvasRef = useRef(null);
     const verificationStepRef = useRef(1);
 
-
+    // State Management
     const [start, setStart] = useState(false);
     const [faceImg, setFaceImg] = useState('');
     const [isSent, setIsSent] = useState(false);
     const [intervalID, setIntervalID] = useState(0);
     const [isModelLoaded, setIsModelLoaded] = useState(false);
-    const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
     const [showMessage, setShowMessage] = useState('Get closer to the camera');
     const [isMultipleFacesDetected, setIsMultipleFacesDetected] = useState(false);
 
 
     const handleLoading = () => {
+        console.log('loading...');
         dispatch(setLoad());
     }
 
-
-    const videoConstraints = {
-        width: 640,
-        height: 480,
-        facingMode: 'user'
-    };
-
     useEffect(() => {
-
-
-
         const loadModels = async () => {
             setIsModelLoaded(false);
             await Promise.all([
-                faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-                faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-                faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-                faceapi.nets.ageGenderNet.loadFromUri("/models"),
-                faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+                faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_PATH),
+                faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_PATH),
+                faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_PATH),
+                faceapi.nets.ageGenderNet.loadFromUri(MODEL_PATH),
+                faceapi.nets.faceExpressionNet.loadFromUri(MODEL_PATH),
             ]);
             setIsModelLoaded(true);
         };
@@ -73,23 +78,18 @@ const FaceID = () => {
 
 
     const handleVideoLoad = async () => {
-
-        faceapi.matchDimensions(canvasRef.current, videoConstraints);
-        handleLoading();
+        faceapi.matchDimensions(canvasRef.current, VIDEO_CONSTRAINTS);
         if (!isSent) {
             const newIntervalID = setInterval(async () => {
                 if (webcamRef.current && webcamRef.current.video) {
-                    const detections = await faceapi.detectAllFaces(
-                        webcamRef.current.video,
-                        new faceapi.TinyFaceDetectorOptions()
-                    )
+                    const detections = await faceapi.detectAllFaces(webcamRef.current.video, FACE_DETECTION_OPTIONS)
                         .withFaceLandmarks()
                         .withFaceDescriptors()
                         .withAgeAndGender()
                         .withFaceExpressions();
 
                     // Canvas reflection
-                    const resizedDetections = faceapi.resizeResults(detections, videoConstraints);
+                    const resizedDetections = faceapi.resizeResults(detections, VIDEO_CONSTRAINTS);
                     if (canvasRef.current) {
                         canvasRef.current.getContext('2d').clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
                     }
@@ -125,7 +125,7 @@ const FaceID = () => {
                         });
                     }
                 }
-            }, 100);
+            }, CAMERA_INTERVAL);
 
             setIntervalID(newIntervalID);
         }
@@ -166,108 +166,120 @@ const FaceID = () => {
         }
     };
 
-    const handleSend = async (face) => {
-        if (face) {
-            stopCamera();
+    // A helper function to handle axios post request
+    async function postRequest(form) {
+        try {
+            return await axios.post(`${process.env.REACT_APP_API_URL}/identification`, form);
+        } catch (error) {
+            console.error(error);
+            Swal.fire('Error', error.message, 'error');
+        }
+    }
 
-            const form = {
-                face_image: face,
-            };
+    // A helper function to handle identified person's response
+    async function handleIdentifiedPerson(response, dispatch) {
+        console.log('identified')
+        const access_token = response.data.access_token;
+        const person = response.data.person;
 
-            var response = null;
-
-
-            try {
-                dispatch(setLoad());
-                setShowMessage('Emotion test passed succesfully! Data is sending...');
-                response = await axios.post(`${process.env.REACT_APP_API_URL}/identification`, form);
-                setShowMessage('Emotion test passed succesfully! Data is sending...');
-            } catch (error) {
-                dispatch(setLoad());
-                console.error(error);
-                Swal.fire('Error', error.message, 'error');
-            } finally {
-                dispatch(setLoad());
-                if (response.status === 200) {
-                    setIsSent(true);
-                    // Person isn't Identified
-                    if (response.data.face_encodings) {
-                        console.log('isnt identified')
-                        dispatch(setFace({
-                            face: response.data.face.face_image,
-                            face_encodings: response.data.face_encodings,
-                        }));
-                        console.log(faceImg)
-                        navigate('/settings');
-                    }
-                    // Person is Identified
-                    else if (response.data.access_token) {
-                        console.log('identified')
-                        const access_token = response.data.access_token;
-                        const person = response.data.person;
-
-                        const result = await Swal.fire({
-                            title: 'Enter your PIN',
-                            input: 'text',
-                            inputLabel: 'PIN',
-                            inputPlaceholder: 'Enter your PIN',
-                            inputAttributes: {
-                                maxlength: 100,
-                                autocapitalize: 'off',
-                                autocorrect: 'off',
-                            },
-                            inputValidator: (value) => {
-                                if (!value) {
-                                    return 'You need to enter your PIN!';
-                                } else if (!/^\d{3,100}$/.test(value)) {
-                                    return 'PIN must be 3-100 digits!';
-                                }
-                            },
-                        });
-
-                        if (result.isConfirmed) {
-                            const pinnedFaceEncodings = generatePinnedFaceEncodings(person.face_encodings, result.value);
-                            try {
-                                const decryptedData = JSON.parse(await decryptData(person.personal_data, person.encrypted_private_key, pinnedFaceEncodings));
-                                dispatch(setUser({
-                                    face: response.data.face.face_image,
-                                    public_key: person.public_key,
-                                    token: access_token,
-                                    face_encodings: person.face_encodings,
-                                    encrypted_public_key: person.encrypted_public_key,
-                                    encrypted_private_key: person.encrypted_private_key,
-                                    id_number: decryptedData.id_number,
-                                    email: decryptedData.email,
-                                    firstname: decryptedData.firstname,
-                                    lastname: decryptedData.lastname,
-                                    nationality: decryptedData.nationality,
-                                    date_of_birth: decryptedData.date_of_birth,
-                                    date_of_issue: decryptedData.date_of_issue,
-                                    date_of_expiry: decryptedData.date_of_expiry,
-                                    phone: decryptedData.phone,
-                                    pin: result.value
-                                }));
-                            } catch (error) {
-                                Swal.fire('Incorrect PIN', 'Try again!', 'error').then((result) => {
-                                    if (result.isConfirmed)
-                                        window.location.reload();
-                                })
-                            }
-                        }
-                    }
-                } else {
-                    console.log(response);
-                    Swal.fire(response.message, 'Try again', 'info');
-                    navigate('/');
+        const result = await Swal.fire({
+            title: 'Enter your PIN',
+            input: 'text',
+            inputLabel: 'PIN',
+            inputPlaceholder: 'Enter your PIN',
+            inputAttributes: {
+                maxlength: 100,
+                autocapitalize: 'off',
+                autocorrect: 'off',
+            },
+            inputValidator: (value) => {
+                if (!value) {
+                    return 'You need to enter your PIN!';
+                } else if (!/^\d{3,100}$/.test(value)) {
+                    return 'PIN must be 3-100 digits!';
                 }
+            },
+        });
+
+        if (result.isConfirmed) {
+            const pinnedFaceEncodings = generatePinnedFaceEncodings(person.face_encodings, result.value);
+            try {
+                const decryptedData = JSON.parse(await decryptData(person.personal_data, person.encrypted_private_key, pinnedFaceEncodings));
+                dispatch(setUser({
+                    face: response.data.face.face_image,
+                    public_key: person.public_key,
+                    token: access_token,
+                    face_encodings: person.face_encodings,
+                    encrypted_public_key: person.encrypted_public_key,
+                    encrypted_private_key: person.encrypted_private_key,
+                    id_number: decryptedData.id_number,
+                    email: decryptedData.email,
+                    firstname: decryptedData.firstname,
+                    lastname: decryptedData.lastname,
+                    nationality: decryptedData.nationality,
+                    date_of_birth: decryptedData.date_of_birth,
+                    date_of_issue: decryptedData.date_of_issue,
+                    date_of_expiry: decryptedData.date_of_expiry,
+                    phone: decryptedData.phone,
+                    pin: result.value
+                }));
+            } catch (error) {
+                Swal.fire('Incorrect PIN', 'Try again!', 'error').then((result) => {
+                    if (result.isConfirmed)
+                        window.location.reload();
+                })
             }
-        } else {
-            dispatch(setLoad());
+        }
+    }
+
+    // A helper function to handle not identified person's response
+    function handleNotIdentifiedPerson(response, dispatch, faceImg) {
+        console.log('isnt identified')
+        dispatch(setFace({
+            face: response.data.face.face_image,
+            face_encodings: response.data.face_encodings,
+        }));
+        console.log(faceImg)
+        navigate('/settings');
+    }
+
+    // Improved handleSend function
+    const handleSend = async (face) => {
+        if (!face) {
             console.log('No image to send.');
-            Swal.fire(response.message, 'Try again', 'error');
+            Swal.fire('No image to send.', 'Try again', 'error');
             navigate('/');
+            return;
+        }
+
+        stopCamera();
+        const form = { face_image: face };
+        const response = await postRequest(form);
+
+        if (!response) {
+            navigate('/');
+            return;
+        }
+
+        if (response.status !== 200) {
+            console.log(response);
+            Swal.fire(response.message, 'Try again', 'info');
+            navigate('/');
+            return;
+        }
+
+        setIsSent(true);
+
+        // Person isn't Identified
+        if (response.data.face_encodings) {
+            handleNotIdentifiedPerson(response, dispatch, faceImg);
+        }
+        // Person is Identified
+        else if (response.data.access_token) {
+            await handleIdentifiedPerson(response, dispatch);
         }
     };
+
 
     const capture = useCallback(() => {
         const face = webcamRef.current.getScreenshot();
@@ -288,7 +300,6 @@ const FaceID = () => {
 
     const handleStart = () => {
         setStart(true);
-        handleLoading();
     };
 
     return (
@@ -300,13 +311,11 @@ const FaceID = () => {
                             audio={false}
                             ref={webcamRef}
                             screenshotFormat="image/jpeg"
-                            videoConstraints={videoConstraints}
+                            videoConstraints={VIDEO_CONSTRAINTS}
                             onUserMedia={handleVideoLoad}
                         />
                         <canvas ref={canvasRef} />
-                        <div className={`${styles.faceid_message} ${showMessage || isMultipleFacesDetected ? styles.message_active : ''}`}>
-                            {showMessage}
-                        </div>
+                        <FaceIDMessage showMessage={showMessage} isMultipleFacesDetected={isMultipleFacesDetected} />
                     </div>
                 </>
                 :
